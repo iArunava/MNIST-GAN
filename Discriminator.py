f53f5a37bc792dfc5aa328d5f09fa68f63c793de
Arunava <learningdroidarunava@gmail.com>
2019-02-01 13:22:37 +0530

    Bug Fixes

--- Discriminator.py
+++ Discriminator.py
@@ -20,11 +20,11 @@ class Discriminator(nn.Module):
         self.output_size = output_size
 
         # Define the required modules for this architecture
-        self.fc1 = nn.Linear(self.input_size, self.hidden_dim*4)
-        self.fc2 = nn.Linear(self.hidden_dim*4, self.hidden_dim*2)
-        self.fc3 = nn.Linear(self.hidden_dim*2, self.hidden_dim)
+        self.linear1 = nn.Linear(self.input_size, self.hidden_dim*4)
+        self.linear2 = nn.Linear(self.hidden_dim*4, self.hidden_dim*2)
+        self.linear3 = nn.Linear(self.hidden_dim*2, self.hidden_dim)
 
-        self.fc4 = nn.Linear(self.hidden_dim, self.output_size)
+        self.linear4 = nn.Linear(self.hidden_dim, self.output_size)
 
         self.dropout = nn.Dropout(0.3)
 
@@ -42,15 +42,15 @@ class Discriminator(nn.Module):
         x = x.view(-1, 784)
 
         # Forward pass
-        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = F.leaky_relu(self.linear1(x), 0.2)
         x = self.dropout(x)
 
-        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = F.leaky_relu(self.linear2(x), 0.2)
         x = self.dropout(x)
 
-        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = F.leaky_relu(self.linear3(x), 0.2)
         x = self.dropout(x)
 
-        out = self.fc4(x)
+        out = self.linear4(x)
 
         return out
--- viz.py
+++ viz.py
@@ -3,8 +3,9 @@ import torch
 
 def view_samples(epochs, samples):
     fig, axes = plt.subplots(figsize=(7, 7), nrows=4, ncols=4, sharey=True, sharex=True)
-    for ax, img in zip(axes.flatten(), samples[epoch]):
+    for ax, img in zip(axes.flatten(), samples[epochs]):
         img = img.detach()
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
         im = ax.imshow(img.cpu().reshape((28, 28)), cmap='Greys_r')
+    plt.show()
